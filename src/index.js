4f201e3621db6af32ed1b0214b6e72c295a617d8
Juliano Souza <juliano.souza.dev@gmail.com>
2021-11-10 15:48:19 -0300

    Listando extrato

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 app.use(express.json())
 
-const customer = []
+const customers = []
 
 app.post('/account', (request, response) => {
   const {name, cpf} = request.body
  
 
-  const  customerExists = customer.some(custumer => custumer.cpf === cpf)
+  const  customerExists = customers.some(custumer => custumer.cpf === cpf)
 
   if(customerExists) {
     return response.status(400).json({error: 'Customer already exists'})
   }
   
 
-  customer.push({
+  customers.push({
     id :uuidv4(),
     name,
     cpf,
@@ -26,6 +26,20 @@ app.post('/account', (request, response) => {
   response.status(201).send()
 
 
+})
+
+
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params
+  
+  const customer = customers.find(custumer => custumer.cpf === cpf)
+
+  if(customer) {
+    return response.json(customer.statement)
+  }
+
+
+  return response.status(400).json({error: 'Customer not found'})
 })
 
 app.listen(3333, () => console.log('Server started on port 3333'))
